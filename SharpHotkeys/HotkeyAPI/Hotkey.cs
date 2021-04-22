0a07ba7e24f6ca777950d6901cbfdb3a99a1dd9b
Chase Roth <cxr6988@g.rit.edu>
2021-04-22 13:40:31 -0400

    Initial Commit

---
+++ SharpHotkeys/HotkeyAPI/Hotkey.cs
@@ -0,0 +1,130 @@
+﻿/*
+ * Copyright (c) Chase Roth <cxr6988@rit.edu>
+ * Licensed under the MIT License. See repository root directory for more info.
+*/
+
+using System;
+using HWND = System.IntPtr;
+
+using WindowsHotkeys.Enumerations;
+using WindowsHotkeys.Native;
+
+namespace WindowsHotkeys
+{
+    /// <summary>
+    /// A <see cref="Hotkey"/> that can be registered / unregistered and listened to.
+    /// Read Source: https://docs.microsoft.com/en-us/windows/win32/winmsg/hooks
+    /// </summary>
+    public class Hotkey : IDisposable
+    {
+        public event Action HotkeyClicked;
+
+        /// <summary>
+        /// Unique identifier for this hot-key used by the operating system.
+        /// </summary>
+        private readonly int keyId;
+
+        /// <summary>
+        /// Handle to a window being used.
+        /// </summary>
+        private readonly HWND hwnd = HWND.Zero;
+
+        #region Properties
+        /// <summary>
+        /// Gets the key that is associated to the hot-key.
+        /// </summary>
+        public Keys Key { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Modifiers Mods { get; private set; }
+        #endregion
+
+        /// <summary>
+        /// Gets a boolean indicating if this hot-key is currently registered.
+        /// </summary>
+        public bool IsRegistered { get; private set; }
+
+        /// <summary>
+        /// Initializes a new <see cref="Hotkey"/> instance and associates it with the given <paramref name="windowHandle"/>
+        /// </summary>
+        /// <param name="key">Key to raise the <see cref="HotkeyClicked"/> event.</param>
+        /// <param name="modifiers"></param>
+        /// <param name="windowHandle"></param>
+        public Hotkey(Keys key, Modifiers modifiers, HWND windowHandle)
+        {
+            keyId = Init(key, modifiers);
+            hwnd = windowHandle;
+        }
+
+        /// <summary>
+        /// Initializes a new <see cref="Hotkey"/> instance and associates it with the calling thread. 
+        /// It is recommended to use your application's dispatcher to ensure this is executed on the main thread.
+        /// </summary>
+        /// <param name="key">Key to raise the <see cref="HotkeyClicked"/> event.</param>
+        /// <param name="modifiers"></param>
+        public Hotkey(Keys key, Modifiers modifiers)
+        {
+            keyId = Init(key, modifiers);
+        }
+
+        /// <summary>
+        /// Shared initialization for both constructors.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="modifiers"></param>
+        /// <returns>HashCode of this <see cref="Hotkey"/> instance.</returns>
+        int Init(Keys key, Modifiers modifiers)
+        {
+            // Multiple flags set
+            if ((key & (key - 1)) != 0)
+                throw new NotSupportedException("The parameter <key> had multiple flags set which would result in the same hot-key needing to respond to key strokes from separate keys. This is currently unsupported.");
+
+            Key = key;
+            Mods = modifiers;
+            return GetHashCode();
+        }
+
+        /// <summary>
+        /// Registers the <see cref="Hotkey"/>.
+        /// </summary>
+        /// <returns>Indication of success.</returns>
+        public virtual bool TryRegisterHotkey()
+        {
+            if (!NativeAPI.RegisterHotKey(hwnd, keyId, (uint)Mods, (uint)Key))
+                return false;   // Failure
+
+            IsRegistered = true;
+            return true;
+        }
+
+        /// <summary>
+        /// Unregisters the <see cref="Hotkey"/>.
+        /// </summary>
+        /// <returns>Indication of success.</returns>
+        public virtual bool TryUnregisterHotkey()
+        {
+            if (!NativeAPI.UnregisterHotKey(hwnd, keyId))
+                return false;   // Failure
+
+            IsRegistered = false;
+            return true;
+        }
+
+        /// <summary>
+        /// Unregisters the hot-key if registered before disposing.
+        /// </summary>
+        public virtual void Dispose()
+        {
+            if (IsRegistered)
+                TryUnregisterHotkey();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected virtual void OnHotkeyClicked()
+            => HotkeyClicked?.Invoke();
+    }
+}
---
+++ SharpHotkeys/Native/NativeAPI.cs
@@ -0,0 +1,91 @@
+﻿/*
+ * Copyright (c) Chase Roth <cxr6988@rit.edu>
+ * Licensed under the MIT License. See repository root directory for more info.
+*/
+
+using System;
+using System.Runtime.InteropServices;
+using HWND = System.IntPtr; // A handle to a window
+using HHOOK = System.IntPtr; // A HANDLE
+using WPARAM = System.IntPtr; // A UINT_PTR which varies in respect to target architecture (x86, x64)
+using LPARAM = System.IntPtr; // A LONG_PTR which varies in respect to target architecture (x86, x64)
+using HOOKPROC = System.IntPtr;
+using HINSTANCE = System.IntPtr;
+using DWORD = System.UInt32;
+using LRESULT = System.IntPtr; // A LONG_PTR which varies in respect to target architecture (x86, x64)
+
+/* Reference Source: https://docs.microsoft.com/en-us/windows/win32/winprog/windows-data-types
+ * Type Rules:
+ * HWND -> IntPtr
+ * LParam -> IntPtr
+ * WParam -> IntPtr, would use UIntPtr but it is not CLS compliant
+ * DWORD -> UInt32
+ * HHOOK -> IntPtr
+ * HOOKPROC -> IntPtr
+ * HINSTANCE -> IntPtr
+ * LRESULT -> IntPtr
+*/
+
+/// <summary>
+/// Contains external functions defined in windows native libraries.
+/// Read Source: https://docs.microsoft.com/en-us/windows/win32/winmsg/about-hooks
+/// </summary>
+namespace SharpHotkeys.Native
+{
+    /// <summary>
+    /// Contains imported user32.dll registration functions.
+    /// </summary>
+    internal static class NativeAPI
+    {
+        const string USER_32 = "user32";
+
+        // https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-registerhotkey
+        [DllImport(USER_32, SetLastError = true)]
+        public static extern bool RegisterHotKey(   
+            HWND hWnd, 
+            int id, 
+            uint fsModifiers,
+            uint vk
+        );
+
+        // https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-unregisterhotkey
+        [DllImport(USER_32, SetLastError = true)]
+        public static extern bool UnregisterHotKey(
+            HWND hWnd,
+            int id
+        );
+
+        // https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-callnexthookex
+        [DllImport(USER_32)]
+        public static extern IntPtr CallNextHookEx(
+          HHOOK hhk,
+          int nCode,
+          WPARAM wParam,
+          LPARAM lParam
+        );
+
+        // https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setwindowshookexa
+        [DllImport(USER_32)]
+        public static extern HHOOK SetWindowsHookExA(
+            int idHook,
+            HOOKPROC lpfn, // Pass method address callback function here for intercepting hooks
+            HINSTANCE hmod,
+            DWORD dwThreadId
+        );
+
+        // https://docs.microsoft.com/en-us/windows/win32/api/errhandlingapi/nf-errhandlingapi-getlasterror
+        // _Post_equals_last_error_ DWORD GetLastError();
+    }
+
+    internal class Delegates
+    {
+        /// <summary>
+        /// Defines the delegate of the callback function for hooks that is given to the <see cref="NativeAPI.SetWindowsHookExA(int, HWND, HWND, DWORD)"/> function.
+        /// </summary>
+        /// <param name="keyId">Key unique identifier given when the hot-key was registered.</param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        public delegate LRESULT HookCallbackDelegate(in int keyId, in WPARAM wParam, in LPARAM lParam);
+    }
+}
