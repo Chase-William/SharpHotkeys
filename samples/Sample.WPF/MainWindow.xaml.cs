41387c6eed7a0d1e83a0a33ac63c61fb2cfffdd6
Chase Roth <cxr6988@g.rit.edu>
2021-04-28 21:16:13 -0400

    switched from .netframework to .core

--- samples/Sample.WPF/MainWindow.xaml.cs
+++ samples/Sample.WPF/MainWindow.xaml.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Windows;
-using System.Windows.Interop;
 using System.Windows.Input;
+using System.Windows.Interop;
 
 using SharpHotkeys.WPF;
-using SharpHotkeys;
 
-namespace Sample
+namespace Sample.WPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -27,18 +26,19 @@ namespace Sample
 
             IntPtr windowHandle = new WindowInteropHelper(this).Handle;
 
-            hotkey = new Hotkey(Key.F6, 
-                ModifierKeys.Shift, 
+            hotkey = new Hotkey(
+                Key.F6,
+                ModifierKeys.Shift,
                 IntPtr.Zero // Or pass in the windowHandle if you want to associate with the window instead of the calling thread.
-                );
+            );
 
             if (!hotkey.TryRegisterHotkey(out uint errCode))
                 throw new Exception("ErrCode Received:" + errCode);
-           
+
             hotkey.HotkeyClicked += delegate
             {
                 MessageBox.Show("Hot-key Clicked!");
-            };            
+            };
         }
 
         protected override void OnClosing(CancelEventArgs e)
